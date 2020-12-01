722cef001f6a7015356cfc0f8f6709b1abdf4304
unknown <quentin@bonaventure.xyz>
2020-12-01 22:59:19 +0100

    Day One

--- day_one.exs
+++ day_one.exs
@@ -1,18 +1,58 @@
 defmodule AOC.DayOne do
 
-  def first_puzzle(expenses) do
-    solution =
-      Enum.reduce_while(expenses, 0, fn sum, acc ->
-        expenses
-        |> Enum.slice(acc..200)
-        |> Enum.find(& &1+sum == 2020)
-        |> continue?({sum, acc})
-      end)
-    IO.puts ~s(The solution to puzzle 1 of day one is: "#{Integer.to_string solution}")
+  def first_puzzle(my_expenses) do
+    my_expenses =  prepare_list(my_expenses, 2)
+    my_expenses_count = Enum.count(my_expenses)
+    Enum.reduce_while(my_expenses, 0, fn expense_one, acc ->
+      my_expenses
+      |> Enum.slice(acc..my_expenses_count)
+      |> Enum.find(& expense_one+&1 == 2020)
+      |> continue?({expense_one, acc})
+    end)
+    |> Integer.to_string
+    |> congratulate_for_one
   end
 
-  def continue?(nil, {_sum, acc}), do: {:cont, acc}
-  def continue?(second_sum, {first_sum, _acc}), do: {:halt, first_sum*second_sum}
+
+  def second_puzzle(my_expenses) do
+    my_expenses =  prepare_list(my_expenses, 3)
+    my_expenses_count = Enum.count(my_expenses)
+    Enum.reduce_while(my_expenses, 0, fn expense_one, acc ->
+      my_expenses
+      |> Enum.slice(acc..my_expenses_count)
+      |> Enum.reduce_while(acc+1, fn expense_two, acc_two ->
+        Enum.slice(my_expenses, acc_two..my_expenses_count)
+        |> Enum.find(& expense_one+expense_two+&1 == 2020)
+        |> continue?({[expense_one, expense_two], acc_two})
+      end)
+      |> case do
+        ss when ss < 200 ->
+          {:cont, acc+1}
+        result ->
+          {:halt, result}
+      end
+    end)
+    |> Integer.to_string
+    |> congratulate_for_two
+  end
+
+
+  def continue?(nil, {_expense_one, acc}), do: {:cont, acc+1}
+  def continue?(expense_three, {[expense_one, expense_two], _acc}), do:
+    {:halt, expense_one*expense_two*expense_three}
+  def continue?(expense_two, {expense_one, _acc}), do:
+    {:halt, expense_one*expense_two}
+
+  def prepare_list(list, numbers_to_find) do
+    sorted_list = Enum.sort(list)
+    lowest_value = Enum.sum(Enum.slice(sorted_list, 0..(numbers_to_find-2)))
+    sorted_list |> Enum.reject(& &1+lowest_value > 2020)
+  end
+
+  def congratulate_for_one(solution), do:
+    IO.puts ~s(Congratulations! The solution to puzzle 1 of day one is: "#{solution}")
+  def congratulate_for_two(solution), do:
+    IO.puts ~s(Congratulations! The solution to puzzle 2 of day one is: "#{solution}")
 
   def my_expenses() do
     [
@@ -218,8 +258,9 @@ defmodule AOC.DayOne do
      1968,
     ]
   end
-
 end
 
 
+
 AOC.DayOne.my_expenses() |> AOC.DayOne.first_puzzle()
+AOC.DayOne.my_expenses() |> AOC.DayOne.second_puzzle()
